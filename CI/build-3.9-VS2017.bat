d21e22e2ebe6c0faf04aae948c580cc4da90ac77
Jan Möbius <moebius@cs.rwth-aachen.de>
2022-03-07 11:45:23 +0100

    Changed creation of venv

--- CI/build-3.9-VS2017.bat
+++ CI/build-3.9-VS2017.bat
@@ -1,5 +1,4 @@
 set PATH=C:\Program Files\Python39;C:\Program Files\Python39\Scripts;C:\Program Files\CMake\bin;%PATH%
-"C:\Program Files\Python39\python.exe" -m venv .
-"C:\Program Files\Python39\Scripts\pip3.exe" install wheel
+virtualenv -p "C:\Program Files\Python39\python.exe" .
 call .\Scripts\activate
 python setup.py bdist_wheel --dist-dir dist3
